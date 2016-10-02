11977362312093041c6e98b84965cb983b7b19b0
root <root@ip-172-31-18-183.ap-south-1.compute.internal>
2016-10-02 23:27:32 +0530

    minor

--- ptah/wsgi.py
+++ ptah/wsgi.py
@@ -7,8 +7,10 @@ PROJECT_PATH_MAIN = '/var/www/site/ptah/ptah/settings'
 sys.path.insert(0, PROJECT_PATH)
 sys.path.append(PROJECT_PATH_MAIN)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ptah.settings'
+
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ptah.settings")
 
 application = get_wsgi_application()
